434a4cf5ed4be051ef88f9473f5a66f7ea4df5ba
@gmgower <gmgower@gmail.com>
2020-07-26 23:04:51 -0500

    Setup files for ancestor project.

--- projects/ancestor/ancestor.py
+++ projects/ancestor/ancestor.py
@@ -1,3 +1,14 @@
+from graph import Graph
 
 def earliest_ancestor(ancestors, starting_node):
-    pass
+    # create a new graph
+    g = Graph()
+
+
+
+
+
+if __name__ == "__main__":
+    ancestors = [(1,3), (2,3), (3,6), (5,6), (5,7), (4,5), (4,8), (8,9), (11,8), (10,1)]
+
+    print(earliest_ancestor(ancestors,3))
